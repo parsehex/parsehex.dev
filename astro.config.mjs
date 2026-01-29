4782afbc9328a26975cffaf93a1adbcf2ded267e
parsehex <1779937+parsehex@users.noreply.github.com>
2026-01-29 15:17:23 -0500

    add mdx, other deps
    other deps mostly for CLI use

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
-import icon from "astro-icon";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import icon from 'astro-icon';
+import astroI18next from 'astro-i18next';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [vue(), icon(), astroI18next()],
+	integrations: [vue(), icon(), astroI18next(), mdx()],
 });
