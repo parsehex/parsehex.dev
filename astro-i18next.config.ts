3da75c03850f162e36d2584a334e2ae40915857d
parsehex <1779937+parsehex@users.noreply.github.com>
2026-02-02 09:29:44 -0500

    remove i18next, add wrangler/cloudflare
    also remove/update other unused parts from the project template

--- astro-i18next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroI18nextConfig } from "astro-i18next";
-
-const config: AstroI18nextConfig = {
-  defaultLocale: "en",
-  locales: ["en", "it"],
-};
-
-export default config;
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import icon from 'astro-icon';
-import astroI18next from 'astro-i18next';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-	integrations: [vue(), icon(), astroI18next(), mdx()],
+	integrations: [vue(), icon(), mdx()],
 });
